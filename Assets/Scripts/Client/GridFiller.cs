18d5884cdc3d4d41498a5ec1f98fe23254454459
Lndael <lndael10@gmail.com>
2021-12-24 12:19:08 +0300

    Finished Grid for Navigator_UI

---
+++ Assets/Scripts/Client/GridFiller.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class GridFiller : MonoBehaviour
+{
+    private GameObject gridCell;
+    [SerializeField] public GameObject grid;
+    [SerializeField] private GameObject gridCellPrefab;
+
+    private char[] letters = Enumerable.Range('A', 'T' - 'A' + 1).Select(c => (char) c).ToArray();
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var columnIndex in letters)
+        {
+            for (int rowIndex = 1; rowIndex <= 20; rowIndex++)
+            {
+                gridCell = Instantiate(gridCellPrefab,grid.transform);
+                gridCell.GetComponent<TextMeshPro>().SetText($"{columnIndex}{rowIndex}");
+            }
+        }
+    }
+}
