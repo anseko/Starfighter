fa9e21b69f448cba279150cd96a6267633ab677b
kozhevnikov.a <kozhevnikov.a@medlinx.online>
2021-12-31 11:55:16 +0300

    changes and fixes

---
+++ Assets/Scripts/Core/FramesLimiter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Core
+{
+    public class FramesLimiter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+            QualitySettings.antiAliasing = 4;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
