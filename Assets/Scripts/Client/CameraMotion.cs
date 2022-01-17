a4896ef14d49734a8240d27c57a6117a581db250
Lndael <lndael10@gmail.com>
2022-01-17 16:05:13 +0300

    Finished dragging

--- Assets/Scripts/Client/CameraMotion.cs
+++ Assets/Scripts/Client/CameraMotion.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Rendering;
 
 namespace Client
 {
@@ -11,9 +10,8 @@ namespace Client
         private Vector3 _offset;
         private Vector3 _translationPoint;
         [SerializeField] private bool _isFollowMode;
-        [SerializeField] private Vector3 lastMousePosition;
-        [SerializeField] private Vector3 currentMousePosition;
-        
+        [SerializeField] private Vector3 _startPosition;
+
         // Start is called before the first frame update
         private void Start()
         {
@@ -25,30 +23,19 @@ namespace Client
         {
             _translationPoint = new Vector3(Input.GetAxis("Horizontal") * _camera.orthographicSize, Input.GetAxis("Vertical") * _camera.orthographicSize, 0);
             _camera.transform.Translate(_translationPoint * -1 * Time.deltaTime);
-        }
-
-        
-
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            if (!_isFollowMode)
-            {
-                lastMousePosition = _camera.ScreenToWorldPoint(eventData.position);
-            }
-        }
-
-        public void OnDrag(PointerEventData eventData)
-        {
-            if (!_isFollowMode)
-            {
-                currentMousePosition = _camera.ScreenToWorldPoint(eventData.position);
-                Vector3 difference = currentMousePosition - lastMousePosition;
-                _camera.transform.Translate(_camera.transform.position+difference);;
-                lastMousePosition = currentMousePosition;
-            }
             
-        }
+            if (Input.GetMouseButtonDown(0))
+            {
+                _startPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            }
 
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 direction = _startPosition - _camera.ScreenToWorldPoint(Input.mousePosition);
+                _camera.transform.position += direction;
+            }
+        }
+        
         private void FollowShip()
         {
             transform.position = Player.transform.position + _offset;
@@ -59,7 +46,7 @@ namespace Client
             if (_isFollowMode) FollowShip();
             else FreeMode();
         }
-
+        
         public void SwitchFollowMode()
         {
             _isFollowMode = !_isFollowMode;
